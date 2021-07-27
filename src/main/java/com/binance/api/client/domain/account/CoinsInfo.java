258c7342f9c1b22118228f03fe9015d80b00bb56
rezazadeh93 <amin.rezazadeh93@gmail.com>
2021-07-27 11:43:42 +0430

    print coins info with toString func

--- src/main/java/com/binance/api/client/domain/account/CoinsInfo.java
+++ src/main/java/com/binance/api/client/domain/account/CoinsInfo.java
@@ -1,10 +1,14 @@
 package com.binance.api.client.domain.account;
 
+import com.binance.api.client.constant.BinanceApiConstants;
 import com.binance.api.client.domain.general.Network;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.List;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CoinsInfo {
     private String coin;
@@ -21,4 +25,24 @@ public class CoinsInfo {
     private Boolean trading;
     private Boolean withdrawAllEnable;
     private Double withdrawing;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
+                .append("coin", coin)
+                .append("depositAllEnable", depositAllEnable)
+                .append("free", free)
+                .append("freeze", freeze)
+                .append("ipoable", ipoable)
+                .append("ipoing", ipoing)
+                .append("isLegalMoney", isLegalMoney)
+                .append("locked", locked)
+                .append("name", name)
+                .append("networkList", networkList)
+                .append("storage", storage)
+                .append("trading", trading)
+                .append("withdrawAllEnable", withdrawAllEnable)
+                .append("withdrawing", withdrawing)
+                .toString();
+    }
 }
--- src/main/java/com/binance/api/client/domain/general/Network.java
+++ src/main/java/com/binance/api/client/domain/general/Network.java
@@ -1,7 +1,9 @@
 package com.binance.api.client.domain.general;
 
+import com.binance.api.client.constant.BinanceApiConstants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -22,4 +24,26 @@ public class Network {
     private Boolean withdrawEnable;
     private Double withdrawFee;
     private Double withdrawMin;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
+                .append("addressRegex", addressRegex)
+                .append("coin", coin)
+                .append("depositDesc", depositDesc)
+                .append("depositEnable", depositEnable)
+                .append("isDefault", isDefault)
+                .append("memoRegex", memoRegex)
+                .append("minConfirm", minConfirm)
+                .append("name", name)
+                .append("network", network)
+                .append("resetAddressStatus", resetAddressStatus)
+                .append("specialTips", specialTips)
+                .append("unLockConfirm", unLockConfirm)
+                .append("withdrawDesc", withdrawDesc)
+                .append("withdrawEnable", withdrawEnable)
+                .append("withdrawFee", withdrawFee)
+                .append("withdrawMin", withdrawMin)
+                .toString();
+    }
 }
