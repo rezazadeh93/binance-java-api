32a62bfa5cfa446d71c88bf3c6f8524af2cbcb86
rezazadeh93 <amin.rezazadeh93@gmail.com>
2021-08-20 19:03:54 +0430

    update withdraw method to supports new arguments

--- src/main/java/com/binance/api/client/BinanceApiAsyncRestClient.java
+++ src/main/java/com/binance/api/client/BinanceApiAsyncRestClient.java
@@ -264,7 +264,8 @@ public interface BinanceApiAsyncRestClient {
    * @param name description/alias of the address
    * @param addressTag Secondary address identifier for coins like XRP,XMR etc.
    */
-  void withdraw(String asset, String address, String amount, String name, String addressTag, BinanceApiCallback<WithdrawResult> callback);
+  void withdraw(String coin, String clientOrderId, String network, String address, String addressTag,
+                String amount, Boolean feeFlag, String name, BinanceApiCallback<WithdrawResult> callback);
 
   /**
    * Fetch account deposit history.
